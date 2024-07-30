b3583abff531fb6051814d38c44c7c6234b91b7a
Atik Rahad <atikrahad1@gmail.com>
2024-07-30 10:12:20 +0600

    setup project with ts eslint prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['**/node_modules/', '.dist/'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+    },
+  },
+);
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import app from "./app";
+
+app.listen(5000, () => {
+    console.log("The server is runnig with 5000")
+})
